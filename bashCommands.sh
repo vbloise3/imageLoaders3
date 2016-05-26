c6df540b90fde42670c67149c28ee7e9739aa4a9
Vince Bloise <vbloise3@verizon.net>
2016-05-25 20:46:41 -0400

    Added IAM policy to bash script

--- bashCommands.sh
+++ bashCommands.sh
@@ -8,3 +8,37 @@ arn:aws:dynamodb:us-west-2:001178231653:table/images
 
 ##zip command
 zip -9 -r ../createThumbnailAndStoreInDB-v1.0.zip *
+
+##policy for creating and storing thumbnail in s3 bucket and dynamodb table
+{
+    "Version":  "2012-10-17",
+    "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "s3:GetObject"
+        ],
+        "Resource": [
+            "arn:aws:s3:::imageload-vbloise/images/*"
+        ]
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            ""s3"PutObject"
+        ],
+        "Resource": [
+            "arn:aws:s3:::imageload-vbloise/thumbs/*"
+        ]
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "dynamodb:PutItem"
+        ],
+        "Resource": [
+            "arn:aws:dynamodb:us-west-2:001178231653:table/images"
+        ]
+    }
+    ]
+}
