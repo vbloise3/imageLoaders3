3c70f9cf63f3279cf8701068e78599b691df2302
Vince Bloise <vbloise3@verizon.net>
2016-05-25 20:11:34 -0400

    Added zip command to bas script

--- bashCommands.sh
+++ bashCommands.sh
@@ -6,3 +6,5 @@ aws s3 mb s3://imageload-vbloise
 ##dynamodb Amazon Resource Name (ARN)
 arn:aws:dynamodb:us-west-2:001178231653:table/images
 
+##zip command
+zip -9 -r ../createThumbnailAndStoreInDB-v1.0.zip *
